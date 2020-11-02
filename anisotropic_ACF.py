2845e7a2998595fb91812c832af04188dc112581
Emily Gould <e.m.gould@sms.ed.ac.uk>
2020-11-02 18:31:57 +0000

    Calculated ACF and added related functions to bubble_tools.py

--- anisotropic_ACF.py
+++ anisotropic_ACF.py
@@ -3,6 +3,7 @@ from skimage import io
 import numpy as np
 import matplotlib.pyplot as pl
 from bubble_tools import split_image
+import pandas as pd
 
 # %% variable setup
 test_file = '/Users/s1101153/OneDrive - University of Edinburgh/Files/bubbles/confocal_data/tunnels/Image73.lsm'
@@ -46,13 +47,81 @@ def xy_autocorr(im):
     return acf
 
 
+# %% functions to extract variables from ACF
+def turnpoints(lst):
+    x = np.array(lst)
+    n = len(x)
+    x0 = x[0]-1.
+    x2 = np.concatenate(([x0], x[:-1]))
+    diffs = x != x2
+    uniques = x[diffs]
+    uniques
+    n2 = len(uniques)
+    poss = np.arange(n)[diffs]
+    exaequos = np.concatenate((poss[1:n2], [n+1]))-poss-1
+    '''
+    at some point need to add in if statements to catch when
+    things are wrong as with the R package
+    '''
+    m = n2-2
+    vals = np.concatenate((np.arange(m)+2, np.arange(m)+1, np.arange(m)))
+    ex = np.array(uniques[vals])
+    ex = np.reshape(ex, (-1, m))
+    ex = np.transpose(ex)
+    peaks = [False]
+    pits = [False]
+    for i in range(m):
+        peaks.append(ex[i, 1] == max(ex[i, ]))
+        pits.append(ex[i, 1] == min(ex[i, ]))
+    peaks.append(False)
+    pits.append(False)
+    tpts = [a or b for a, b in zip(peaks, pits)]
+    if sum(tpts) == 0:
+        tppos = np.nan
+        peaks = [False]*n2
+        pits = [False]*n2
+    else:
+        tppos = (poss+exaequos)[tpts]
+    return tppos
+
+
+def acf_variables(acf_x, acf_y):
+    x_size = len(acf_x)
+    y_size = len(acf_y)
+
+    acf_x = acf_x[x_size//2+1:]
+    acf_y = acf_y[y_size//2+1:]
+
+    grad_x = (acf_x[4] - acf_x[0])/5
+    grad_y = (acf_y[4] - acf_y[0])/5
+    grad_diff = grad_x - grad_y
+
+    tp_x = turnpoints(acf_x)[0]
+    tp_y = turnpoints(acf_y)[0]
+    tp_diff = tp_x - tp_y
+
+    output = {'grad_x': grad_x,
+              'grad_y': grad_y,
+              'grad_diff': grad_diff,
+              'tp_x': tp_x,
+              'tp_y': tp_y,
+              'tp_diff': tp_diff}
+    return pd.Series(output)
+
+
 # %% try out functions
+var_list = []
 for i in range(len(im_list)):
     image = im_list[i]
     y_size, x_size = image.shape
     acf = xy_autocorr(image)
     acf_x = x_profile(acf)
     acf_y = y_profile(acf)
+
+    vars = acf_variables(acf_x, acf_y)
+    dist = pd.Series({'distance': d_list[i]})
+    vars = vars.append(dist)
+    var_list.append(vars)
 
     pl.imshow(image)
     pl.title('Original image, distance = '+str(d_list[i]))
@@ -85,3 +154,4 @@ for i in range(len(im_list)):
     pl.title('ACF centre, distance = '+str(d_list[i]))
     pl.legend()
     pl.show()
+pd.concat(var_list, axis=1)
--- bubble_tools.py
+++ bubble_tools.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as pl
-from skimage import io
+import pandas as pd
+from scipy import fft
+# from skimage import io
 
 # %% test variables
 # test_file = '/Users/s1101153/OneDrive - University of Edinburgh/Files/bubbles/confocal_data/tunnels/Image73.lsm'
@@ -13,7 +15,7 @@ from skimage import io
 # shift = 16
 
 
-# %%
+# %% functions to split image into chunks
 def find_closest(im_centre, hole_locs):
     dists = []
     for px in hole_locs:
@@ -62,4 +64,101 @@ def split_image(im, seg_im, chunk_x, chunk_y, shift, plot_ims=False):
             y_max += shift
         x_min += shift
         x_max += shift
-    return im_list, centre_list, dist_list
+        dat = pd.DataFrame([im_list, centre_list, dist_list])
+        dat = dat.transpose()
+        dat.columns = ['chunk_im', 'chunk_loc', 'distance']
+    return dat
+
+
+# %% functions to calculate ACF
+def x_profile(im):
+    x_profile = []
+    y, x = np.indices(im.shape)
+    for i in np.unique(x):
+        x_profile.append(np.mean(im[x == i]))
+    return x_profile
+
+
+def y_profile(im):
+    y_profile = []
+    y, x = np.indices(im.shape)
+    for i in np.unique(y):
+        y_profile.append(np.mean(im[y == i]))
+    return y_profile
+
+
+def xy_autocorr(im):
+    ft = fft.fft2(im)
+    ft_conj = np.conj(ft)
+    m, n = ft.shape
+    acf = np.real(fft.ifft2(ft*ft_conj))
+    acf = np.roll(acf, -m//2+1, axis=0)
+    acf = np.roll(acf, -n//2+1, axis=1)
+    return acf
+
+
+# %% functions to extract variables from ACF
+def turnpoints(lst):
+    x = np.array(lst)
+    n = len(x)
+    x0 = x[0]-1.
+    x2 = np.concatenate(([x0], x[:-1]))
+    diffs = x != x2
+    uniques = x[diffs]
+    uniques
+    n2 = len(uniques)
+    poss = np.arange(n)[diffs]
+    exaequos = np.concatenate((poss[1:n2], [n+1]))-poss-1
+    '''
+    at some point need to add in if statements to catch when
+    things are wrong as with the R package
+    '''
+    m = n2-2
+    vals = np.concatenate((np.arange(m)+2, np.arange(m)+1, np.arange(m)))
+    ex = np.array(uniques[vals])
+    ex = np.reshape(ex, (-1, m))
+    ex = np.transpose(ex)
+    peaks = [False]
+    pits = [False]
+    for i in range(m):
+        peaks.append(ex[i, 1] == max(ex[i, ]))
+        pits.append(ex[i, 1] == min(ex[i, ]))
+    peaks.append(False)
+    pits.append(False)
+    tpts = [a or b for a, b in zip(peaks, pits)]
+    if sum(tpts) == 0:
+        tppos = np.nan
+        peaks = [False]*n2
+        pits = [False]*n2
+    else:
+        tppos = (poss+exaequos)[tpts]
+    return tppos
+
+
+def acf_variables(acf_x, acf_y):
+    x_size = len(acf_x)
+    y_size = len(acf_y)
+
+    acf_x = acf_x[x_size//2+1:]
+    acf_y = acf_y[y_size//2+1:]
+
+    grad_x = (acf_x[4] - acf_x[0])/5
+    grad_y = (acf_y[4] - acf_y[0])/5
+    grad_diff = grad_x - grad_y
+
+    try:
+        tp_x = turnpoints(acf_x)[0]
+        tp_y = turnpoints(acf_y)[0]
+        tp_diff = tp_x - tp_y
+    except TypeError:
+        tp_x = None
+        tp_y = None
+        tp_diff = None
+
+    output = {'grad_x': grad_x,
+              'grad_y': grad_y,
+              'grad_diff': grad_diff,
+              'tp_x': tp_x,
+              'tp_y': tp_y,
+              'tp_diff': tp_diff}
+    return pd.Series(output)
